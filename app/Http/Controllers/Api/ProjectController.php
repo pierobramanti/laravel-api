eacd5438075c44e8782fbf3c3a1c5a2acf35deb6
pierobramanti <piero.bramanti94@gmail.com>
2024-10-14 17:55:34 +0200

    [UPD] Add API routes and ProjectController for project management. Milestone 1 Done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project; 
 
 class ProjectController extends Controller
 {
-    //
+    public function index() {
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
