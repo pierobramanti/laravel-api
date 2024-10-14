4e5a41c13488f91e8c31367068faf1503878010d
pierobramanti <piero.bramanti94@gmail.com>
2024-10-14 18:25:43 +0200

    [UPD] Successfully tested API with Thunder Client - Milestone 2 completed

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// API WORKS //
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
